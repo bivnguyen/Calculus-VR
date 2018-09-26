c3b4b5df1ef5c7b3c66184ae3da2f676fd278af1
bivnguyen <bnguyen42@horizon.csueastbay.edu>
2018-09-26 16:30:28 -0700

    Test Graphs

---
+++ Assets/Scripts/DrawAxes.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawAxes : MonoBehaviour {
+
+    public Transform pointPrefab;
+    private Transform xPoint, yPoint;
+    float x, y;
+    // Use this for initialization
+    void Awake()
+    {
+        int i = 0;
+        x = 0;
+        y = 0;
+        xPoint = Instantiate(pointPrefab);
+        yPoint = Instantiate(pointPrefab);
+        xPoint.localPosition = transform.position;
+        yPoint.localPosition = transform.position;
+        while(i <1000){
+            x = i * .1f;
+            y = i * .1f;
+            xPoint = Instantiate(pointPrefab);
+            xPoint.localPosition = transform.position + new Vector3(x, 0, 0);
+            yPoint = Instantiate(pointPrefab);
+            yPoint.localPosition = transform.position + new Vector3(0, y, 0);
+            i++;
+        }
+    }
+
+    // Update is called once per frame
+
+}
---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour {
+
+    public Transform pointPrefab;
+    private GetGraphValues graphValues;
+    private Transform point;
+    float time;
+	// Use this for initialization
+	void Start () {
+        GameObject sphereObject = GameObject.FindWithTag("Ball");
+        graphValues = sphereObject.GetComponent<GetGraphValues>();
+        time = 0;
+        point = Instantiate(pointPrefab);
+        point.localPosition = transform.position + new Vector3(time, graphValues.graph_values.y, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time = time + Time.deltaTime;
+        point = Instantiate(pointPrefab);
+        point.localPosition = transform.position + new Vector3(time, graphValues.graph_values.y, 0);
+
+    }
+}
